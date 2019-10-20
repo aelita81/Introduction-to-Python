792e4a1b7df29a85915ce514c51228717572e7da
aelita81 <aelitka@gmail.com>
2019-10-21 02:10:51 +0400

    Practical1 added

---
+++ PR1.py
@@ -0,0 +1,20 @@
+course = "Intro to Python"
+print (course)
+student = "Aelita Sargsyan"
+print (student)
+nickname = "Aelita"
+print (nickname)
+grade = 25
+int(grade)
+python_expert = False
+print (course)
+print (student)
+print (nickname)
+print (grade)
+print (python_expert)
+import math
+AB = 3
+AC = 4
+BC_sq = (AB**2 + AC**2) #BC squared
+BC = math.sqrt (BC_sq)
+print (BC)
